410fbfaa6cdc8bf10eb03d66cf37b1d9ff50c8c5
Zar Zakharov <severenit@Zars-MacBook-Pro.local>
2017-01-11 16:58:21 +0300

    Add reducer and redux UPDATE COMMIT

--- src/Hero.js
+++ src/Hero.js
@@ -3,14 +3,15 @@ import LikeCounter from './LikeCounter';
 
 class Hero extends Component {
   render() {
+    const _item = this.props.item;
     return (
         <div className="col-xs-6 col-md-3">
           <div className="thumbnail">
-            <img src={this.props.item.imageUrl} alt={this.props.item.title}/>
+            <img src={_item.imageUrl} alt={_item.title}/>
             <div className="caption">
-              <h3>{this.props.item.title}</h3>
-              <p>{this.props.item.subtitle}</p>
-              <LikeCounter count={this.props.item.likes} onCount={this.props.item.onAddLike}/>
+              <h3>{_item.title}</h3>
+              <p>{_item.subtitle}</p>
+              <LikeCounter count={_item.likes} onCount={_item.onAddLike}/>
             </div>
           </div>
         </div>
